5d77a161c1e3a47a7eceeb6533428e2e4e0b5f29
kasparas <git@kasparas.io>
2025-01-10 15:41:00 +0200

    chore: remove accidentally commited node_modules and dist from github

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const brokenBorder: {
-    handler: import("tailwindcss/types/config").PluginCreator;
-    config?: Partial<import("tailwindcss/types/config").Config>;
-};
-export default brokenBorder;
